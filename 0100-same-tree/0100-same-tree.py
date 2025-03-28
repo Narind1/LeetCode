6a0500e9e6863df9fe6737a0a85c1861b96f0139
NARIND VERMA <138598793+Narind1@users.noreply.github.com>
2025-03-28 22:00:44 +0530

    Time: 0 ms (100%), Space: 12.7 MB (15.58%) - LeetHub

---
+++ 0100-same-tree/0100-same-tree.py
@@ -0,0 +1,17 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:
+    def isSameTree(self, p, q):
+        if not p and not q:  # Both trees are empty
+            return True
+        if not p or not q:  # One tree is empty
+            return False
+        if p.val != q.val:  # Values do not match
+            return False
+        
+        # Recursively check left and right subtrees
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
