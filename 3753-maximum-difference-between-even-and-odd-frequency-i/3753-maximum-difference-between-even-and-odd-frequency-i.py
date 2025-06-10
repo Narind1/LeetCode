926a2631351900f04ca0f4f93190586de72920a8
NARIND VERMA <138598793+Narind1@users.noreply.github.com>
2025-06-10 23:17:43 +0530

    Time: 17 ms (7.41%), Space: 12.5 MB (11.11%) - LeetHub

---
+++ 3753-maximum-difference-between-even-and-odd-frequency-i/3753-maximum-difference-between-even-and-odd-frequency-i.py
@@ -0,0 +1,14 @@
+from collections import Counter
+
+class Solution(object):
+    def maxDifference(self, s):
+        freq = Counter(s)
+        odd_freqs = [count for count in freq.values() if count % 2 == 1]
+        even_freqs = [count for count in freq.values() if count % 2 == 0]
+        
+        max_diff = float('-inf')
+        for odd in odd_freqs:
+            for even in even_freqs:
+                max_diff = max(max_diff, odd - even)
+                
+        return max_diff
